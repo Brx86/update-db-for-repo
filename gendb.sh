bf53cfb09e786014d3daf676790530d753a34687
brx86 <1124852137@qq.com>
2021-06-20 15:35:54 +0800

    update

--- gendb.sh
+++ gendb.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-set -euo pipefail
 
 mkdir aya
+ls -la
+pwd
 urls=`curl https://github.com/Brx86/repo/releases/tag/$1 -s|grep "/Brx86/repo/releases/download/$1/"|awk -F '"|"' '{print$2}'`
+
 for i in ${urls[*]}; do 
 wget https://github.com/$i -P aya/
 done
+
 cd aya
 repo-add aya.db.tar.gz ./*
