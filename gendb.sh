6d529c433a066585b789bbbb39ef845f8f475d86
brx86 <1124852137@qq.com>
2021-06-20 16:19:32 +0800

    update

--- gendb.sh
+++ gendb.sh
@@ -2,11 +2,8 @@
 
 mkdir aya -p
 
-#urls=$(curl https://github.com/Brx86/repo/releases/tag/x86_64 -s|grep "/Brx86/repo/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
-urls=(/Brx86/repo/releases/download/x86_64/pepper-flash-32.0.0.465-1-x86_64.pkg.tar.zst
-/Brx86/repo/releases/download/x86_64/ttf-ms-fonts-2.0-12-any.pkg.tar.zst
-/Brx86/repo/releases/download/x86_64/woeusb-5.1.2-1-any.pkg.tar.zst
-)
+urls=$(curl https://github.com/Brx86/repo/releases/tag/x86_64 -s|grep "/Brx86/repo/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
+
 for i in ${urls[*]}; do 
 pkg=$(echo $i|awk -F '/' '{print$7}')
 echo Downloading $pkg
