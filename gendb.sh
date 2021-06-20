8423a96ca9c6c2c4726b4cdd0fa9547c355a1ba4
brx86 <1124852137@qq.com>
2021-06-20 16:14:31 +0800

    update

--- gendb.sh
+++ gendb.sh
@@ -3,7 +3,7 @@
 mkdir aya -p
 
 #urls=$(curl https://github.com/Brx86/repo/releases/tag/x86_64 -s|grep "/Brx86/repo/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
-urls=(/Brx86/repo/releases/download/x86_64/todesk-bin-1.2.5-1-x86_64.pkg.tar.zst
+urls=(/Brx86/repo/releases/download/x86_64/pepper-flash-32.0.0.465-1-x86_64.pkg.tar.zst
 /Brx86/repo/releases/download/x86_64/ttf-ms-fonts-2.0-12-any.pkg.tar.zst
 /Brx86/repo/releases/download/x86_64/woeusb-5.1.2-1-any.pkg.tar.zst
 )
