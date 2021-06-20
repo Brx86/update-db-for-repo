6ed109f8bad666427b3b9d7da9d694b7436720e6
brx86 <1124852137@qq.com>
2021-06-20 16:11:52 +0800

    update

--- gendb.sh
+++ gendb.sh
@@ -2,7 +2,11 @@
 
 mkdir aya -p
 
-urls=$(curl https://github.com/Brx86/repo/releases/tag/x86_64 -s|grep "/Brx86/repo/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
+#urls=$(curl https://github.com/Brx86/repo/releases/tag/x86_64 -s|grep "/Brx86/repo/releases/download/x86_64/"|awk -F '"|"' '{print$2}')
+urls=(/Brx86/repo/releases/download/x86_64/todesk-bin-1.2.5-1-x86_64.pkg.tar.zst
+/Brx86/repo/releases/download/x86_64/ttf-ms-fonts-2.0-12-any.pkg.tar.zst
+/Brx86/repo/releases/download/x86_64/woeusb-5.1.2-1-any.pkg.tar.zst
+)
 for i in ${urls[*]}; do 
 pkg=$(echo $i|awk -F '/' '{print$7}')
 echo Downloading $pkg
